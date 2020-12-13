d56fce7afc402dfbd67faf2069a93b8f56b3d253
deguchi <deguchik@gmail.com>
2020-12-13 11:14:29 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import EXIF from 'exif-js';
 
+const form:any = document.querySelector('form');
 const input:any = document.getElementById('image1');
-const label:any = document.querySelector('label');
 
 const base64ToArrayBuffer = (base64:any) => {
   base64 = base64.replace(/^data\:([^\;]+)\;base64,/gmi, '');
@@ -17,32 +17,36 @@ const base64ToArrayBuffer = (base64:any) => {
 let reader:any = null;
 input.addEventListener('change', () => {
   if (input.files && input.files[0]) {
-    reader = new FileReader();
-    reader.onload = (e:any) => {
-      label.style.backgroundImage = `url(${e.target.result})`;
-
-      //回転対応 ,  回転具合を見てlabelを回転
-      const arrayBuffer = base64ToArrayBuffer(reader.result);
-      const exif = EXIF.readFromBinaryFile(arrayBuffer);
-      console.log(exif)
-      // let rotate = 0;
-      // if (exif && exif.Orientation) {
-      //     console.log(exif.Orientation)
-      //   switch (exif.Orientation) {
-      //     case 3:
-      //       rotate = 180;
-      //       break;
-      //     case 6:
-      //       rotate = 90;
-      //       break;
-      //     case 8:
-      //       rotate = -90;
-      //       break;
-      //   }
-      // }
-      // label.style.transform = `rotate(${rotate}deg)`;
-      // label.style.webkitTransform = `rotate(${rotate}deg)`;
-    }
-    reader.readAsDataURL(input.files[0]);
+    [...input.files].map((file: any) => {
+      reader = new FileReader();
+      reader.onload = (e:any) => {
+        const label = document.createElement('label')
+        label.style.backgroundImage = `url(${e.target.result})`;
+        form.appendChild(label);
+  
+        //回転対応 ,  回転具合を見てlabelを回転
+        const arrayBuffer = base64ToArrayBuffer(reader.result);
+        const exif = EXIF.readFromBinaryFile(arrayBuffer);
+        console.log(exif)
+        // let rotate = 0;
+        // if (exif && exif.Orientation) {
+        //     console.log(exif.Orientation)
+        //   switch (exif.Orientation) {
+        //     case 3:
+        //       rotate = 180;
+        //       break;
+        //     case 6:
+        //       rotate = 90;
+        //       break;
+        //     case 8:
+        //       rotate = -90;
+        //       break;
+        //   }
+        // }
+        // label.style.transform = `rotate(${rotate}deg)`;
+        // label.style.webkitTransform = `rotate(${rotate}deg)`;
+      }
+      reader.readAsDataURL(file);
+    })
   }
 });
