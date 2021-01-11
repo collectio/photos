7a9cdea603196bf9e88c226ee6b224824204a8f4
deguchi <deguchik@gmail.com>
2021-01-11 14:55:58 +0900

    update

--- src/Home.tsx
+++ src/Home.tsx
@@ -37,56 +37,88 @@ class Home extends React.Component<Props, State> {
     setInputRef(element: HTMLInputElement) {
         this.input = element;
     }
-    componentDidMount() {
-        let reader:any = null;
-        if (this.input) {
-            this.input.addEventListener('change', () => {
-                if (this.input && this.input.files) {
-                    let photos: PhotoType[] = [];
-                    Array.from(this.input.files).map((file: any) => {
-                        reader = new FileReader();
-                        reader.onload = (e:any) => {
-                            photos.push({
-                                image: e.target.result,
-                                game: {
-                                    id: null,
-                                    title: ''
-                                }
-                            });
-                            if (this.input?.files?.length === photos.length) {
-                                const album: AlbumType = {
-                                    title: 'ある日のボードゲーム会',
-                                    date: '2020/12/13',
-                                    photos: photos
-                                }
-                                this.props.setAlbums(album)
-                            }
-                            //回転対応 ,  回転具合を見てlabelを回転
-                            const arrayBuffer = base64ToArrayBuffer(reader.result);
-                            const exif = EXIF.readFromBinaryFile(arrayBuffer);
-                            console.log(exif)
-                            // let rotate = 0;
-                            // if (exif && exif.Orientation) {
-                            //     console.log(exif.Orientation)
-                            //   switch (exif.Orientation) {
-                            //     case 3:
-                            //       rotate = 180;
-                            //       break;
-                            //     case 6:
-                            //       rotate = 90;
-                            //       break;
-                            //     case 8:
-                            //       rotate = -90;
-                            //       break;
-                            //   }
-                            // }
-                            // label.style.transform = `rotate(${rotate}deg)`;
-                            // label.style.webkitTransform = `rotate(${rotate}deg)`;
+    loadImage() {
+        if (this.input && this.input.files) {
+            let reader:any = null;
+            let photos: PhotoType[] = [];
+            Array.from(this.input.files).map((file: any) => {
+                reader = new FileReader();
+                reader.onload = async (e:any) => {
+                    photos.push({
+                        image: await this.resizeImage(e.target.result) as string,
+                        game: {
+                            id: null,
+                            title: ''
                         }
-                        reader.readAsDataURL(file);
                     });
+                    if (this.input?.files?.length === photos.length) {
+                        const album: AlbumType = {
+                            title: 'ある日のボードゲーム会',
+                            date: '2020/12/13',
+                            photos: photos
+                        }
+                        this.props.setAlbums(album)
+                    }
+                    //回転対応 ,  回転具合を見てlabelを回転
+                    // const arrayBuffer = base64ToArrayBuffer(reader.result);
+                    // const exif = EXIF.readFromBinaryFile(arrayBuffer);
+                    // console.log(exif)
+                    // let rotate = 0;
+                    // if (exif && exif.Orientation) {
+                    //     console.log(exif.Orientation)
+                    //   switch (exif.Orientation) {
+                    //     case 3:
+                    //       rotate = 180;
+                    //       break;
+                    //     case 6:
+                    //       rotate = 90;
+                    //       break;
+                    //     case 8:
+                    //       rotate = -90;
+                    //       break;
+                    //   }
+                    // }
+                    // label.style.transform = `rotate(${rotate}deg)`;
+                    // label.style.webkitTransform = `rotate(${rotate}deg)`;
                 }
+                reader.readAsDataURL(file)
             });
+        }
+    }
+
+    resizeImage(base64: string) {
+        return new Promise((resolve, reject) => {
+            const MIN_SIZE = 1024
+            const canvas = document.createElement('canvas')
+            const ctx = canvas.getContext('2d')
+            if (ctx) {
+                const img = new Image();
+                img.crossOrigin = 'Anonymous'
+                img.onload = (event) => {
+                    const image = event.target as HTMLImageElement
+                    let dstWidth, dstHeight
+                    if (image.width > image.height) {
+                        dstWidth = MIN_SIZE;
+                        dstHeight = image.height * MIN_SIZE / image.width
+                    } else {
+                        dstHeight = MIN_SIZE;
+                        dstWidth = image.width * MIN_SIZE / image.height
+                    }
+                    canvas.width = dstWidth
+                    canvas.height = dstHeight
+                    ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, dstWidth, dstHeight)
+                    resolve(canvas.toDataURL())
+                }
+                img.src = base64
+            } else {
+                reject()
+            }
+        })
+    }
+
+    componentDidMount() {
+        if (this.input) {
+            this.input.addEventListener('change', this.loadImage.bind(this));
         }
     }
     render() {
