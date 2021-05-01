0c90d63a01c99b471985550fe883cd62ef67ddd5
deguchi <deguchik@gmail.com>
2021-05-01 11:47:53 +0900

    update

--- src/Select.tsx
+++ src/Select.tsx
@@ -45,6 +45,7 @@ class Select extends React.Component<Props & RouteComponentProps, State> {
         };
     }
     componentDidMount() {
+        scrollTo(0, 0)
         if (this.textInput) {
             this.textInput.addEventListener('focus', () => {
                 scrollTo(0, 140)
