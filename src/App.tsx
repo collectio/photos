c0383133a936b45ea57d75f9a4bd75d1c8fe9df6
deguchi <deguchik@gmail.com>
2021-06-27 16:44:36 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import firebase, { db } from './index'
 import React, { ReactPropTypes } from "react";
 import {
-    BrowserRouter as Router,
+    HashRouter as Router,
     Switch,
     Route,
     Link
--- src/index.tsx
+++ src/index.tsx
@@ -6,8 +6,6 @@ import "firebase/storage";
 
 import firebaseConfig from '../firebaseConfig.json'
 
-console.log('collectio photo')
-
 firebase.initializeApp(firebaseConfig);
 export const db = firebase.firestore();
 // if (location.hostname === 'localhost' || location.hostname.match(/192\.168\.0\.\w+$/)) {
@@ -18,14 +16,8 @@ export const analytics = firebase.analytics();
 
 export default firebase
 
-
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './App';
 
-console.log('collectio photo init')
-
-
 ReactDOM.render(<App />, document.getElementById("app"));
-
-console.log('collectio photo done')
